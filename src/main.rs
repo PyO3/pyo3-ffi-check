c216726741af7ec97c32f53f6dbfc5da800251f5
Alex Gaynor <alex.gaynor@gmail.com>
2022-06-02 11:45:16 -0400

    Check for alignment, in addition to sizes

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,10 @@ fn main() {
         ($name:ident) => {{
             let pyo3_ffi_size = std::mem::size_of::<pyo3_ffi::$name>();
             let bindgen_size = std::mem::size_of::<bindings::$name>();
+            
+            let pyo3_ffi_align = std::mem::align_of::<pyo3_ffi::$name>();
+            let bindgen_align = std::mem::align_of::<bindings::$name>();
+            
             // Check if sizes differ, but ignore zero-sized types (probably "opaque" in pyo3-ffi)
             if pyo3_ffi_size == 0 {
                 println!(
@@ -20,6 +24,14 @@ fn main() {
                     stringify!($name),
                     pyo3_ffi_size,
                     bindgen_size
+                );
+            } else if pyo3_ffi_align != bindgen_align {
+                failed = true;
+                println!(
+                    "error: alignment of {} differs between pyo3_ffi ({}) and bindgen ({})",
+                    stringify!($name),
+                    pyo3_ffi_align,
+                    bindgen_align
                 );
             }
         }};
